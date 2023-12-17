e717864376f3d409584d96be07019ef9558d2a38
oussabi-tarek <tarek.oussabi@edu.uca.ma>
2023-12-17 20:24:48 +0100

    fix issues

--- src/main/java/com/microservices/notificationservice/OrderPlacedEvent.java
+++ src/main/java/com/microservices/notificationservice/OrderPlacedEvent.java
@@ -1,10 +1,13 @@
 package com.microservices.notificationservice;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
 @AllArgsConstructor
-@Getter
 public class OrderPlacedEvent {
   private String orderNumber;
 }
